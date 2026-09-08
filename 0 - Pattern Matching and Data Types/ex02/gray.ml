01d1cf95b33188cd5be02050be9096bbc62f4d52
FG <44943107+djfg0@users.noreply.github.com>
2026-09-08 13:02:47 +0200

    Updated ex02/gray.ml to be more functionnal...
    Added pattern matching, avoided else ifs, avoided String.make of 1 char, used print_newline instead of print_char, ...

--- 0 - Pattern Matching and Data Types/ex02/gray.ml
+++ 0 - Pattern Matching and Data Types/ex02/gray.ml
@@ -6,48 +6,53 @@
 (*   By: fguarrac <fguarrac@student.42.fr>          +#+  +:+       +#+        *)
 (*                                                +#+#+#+#+#+   +#+           *)
 (*   Created: 2026/09/07 18:15:37 by fguarrac          #+#    #+#             *)
-(*   Updated: 2026/09/07 18:15:38 by fguarrac         ###   ########.fr       *)
+(*   Updated: 2026/09/08 12:56:31 by fguarrac         ###   ########.fr       *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let gray (bits : int) : unit =
     if bits <= 0 then
-        print_char '\n'
+        print_newline ()
     else
         begin
             let xor_string (s1 : string) (s2 : string)  : string =
-                let rec loop (i: int) (s: string) : string =
+                let rec xor_loop (i: int) (s: string) : string =
                     if i = bits then
                         s
-                    else if String.get s1 i <> String.get s2 i then
-                        loop (i + 1) (s ^ String.make 1 '1')
                     else
-                        loop (i + 1) (s ^ String.make 1 '0')
-                in loop 0 ""
+                        begin
+                            if String.get s1 i <> String.get s2 i then
+                                xor_loop (i + 1) (s ^ "1")
+                            else
+                                xor_loop (i + 1) (s ^ "0")
+                        end
+                in xor_loop 0 ""
             in
 
             let rhs_string (s1 : string) : string = 
-                (String.make 1 '0') ^ String.sub s1 0 (bits - 1)
+                "0" ^ String.sub s1 0 (bits - 1)
             in
 
             let inc_string (current : string) : string =
                 let rec inc_loop index carry incremented =
                     if index < 0 then
                         incremented
-                    else if carry = true then
-                        begin
-                            if String.get current index = '0' then
-                                inc_loop (index - 1) false incremented ^ (String.make 1 '1')
-                            else
-                                inc_loop (index - 1) true incremented ^ (String.make 1 '0')
-                        end
                     else
-                        begin
-                            if String.get current index = '0' then
-                                inc_loop (index - 1) false incremented ^ (String.make 1 '0')
-                            else
-                                inc_loop (index - 1) false incremented ^ (String.make 1 '1')
-                        end
+                        match carry with
+                        | true ->
+                                    begin
+                                        if String.get current index = '0' then
+                                            inc_loop (index - 1) false incremented ^ "1"
+                                        else
+                                            inc_loop (index - 1) true incremented ^ "0"
+                                    end
+                        | false ->
+                                    begin
+                                        if String.get current index = '0' then
+                                            inc_loop (index - 1) false incremented ^ "0"
+                                        else
+                                            inc_loop (index - 1) false incremented ^ "1"
+                                    end
                 in inc_loop (bits - 1) true ""
             in
 
@@ -56,7 +61,7 @@ let gray (bits : int) : unit =
             let rec gray_loop (gray_val: string) (current_val : string) =
                 print_string gray_val;
                 if current_val = stop then
-                    print_char '\n'
+                    print_newline ()
                 else
                     begin
                         let next_val = inc_string current_val
